9cd851ae28e088966666e6689bedd6c674d04703
William Billingsley <wbillingsley@cantab.net>
2024-12-21 15:02:24 +1100

    star 4

--- src/day2.rs
+++ src/day2.rs
@@ -47,7 +47,53 @@ fn part1() {
 
 
 fn part2() {
-   // not yet
+    let input = read_input("input.txt".to_string());
+
+    let grid = input.into_iter().map(|l| {
+        let nums = l.split_whitespace().map(|s| {
+            s.parse::<i32>().expect("Couldn't parse {s}")
+        });
+        nums.collect::<Vec<_>>()
+    }).collect::<Vec<_>>();
+
+    fn safe_ascending(vec: &Vec<(i32, i32)>) -> bool {
+        vec.into_iter().all(|(a, b)| {
+            b > a && b - a <= 3    
+        })
+    }
+
+    fn safe_descending(vec: &Vec<(i32, i32)>) -> bool {
+        vec.into_iter().all(|(a, b)| {
+            a > b && a - b <= 3    
+        })
+    }
+
+    fn safe(vec: &Vec<(i32, i32)>) -> bool {
+        safe_ascending(vec) || safe_descending(vec)
+    }
+
+    fn pairs(vec: &Vec<i32>) -> Vec<(i32, i32)> {
+        vec.into_iter().zip(vec.into_iter().skip(1)).map(|(a, b)| (*a, *b)).collect::<Vec<_>>()
+    }
+
+    fn damped_safe(vec: &Vec<i32>) -> bool {
+        let mut indices = (0 .. vec.len()).into_iter();
+
+        indices.any(|x| {
+
+            let removed = vec.into_iter().enumerate().filter_map(|(i, &e)| if i == x { None } else { Some(e) } ).collect();
+            dbg!(&removed);
+            safe(&pairs(&removed))
+        })
+
+    }
+
+    let s = grid.into_iter().filter(|arr| {
+        safe(&pairs(&arr)) || damped_safe(arr)
+    }).count();
+
+    dbg!(s);
+
 }
 
 pub fn day2() {
