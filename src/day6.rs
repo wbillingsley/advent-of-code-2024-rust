817e2a129950dafefd571b70031077d06a6d0a6f
William Billingsley <wbillingsley@cantab.net>
2024-12-22 18:01:24 +1100

    star 12

--- src/day6.rs
+++ src/day6.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::collections::HashMap;
 use std::collections::HashSet;
 
 
@@ -13,8 +12,8 @@ fn read_input(file_path: String) -> Vec<String> {
     vec
 }
 
-fn obstructions(text:&Vec<String>) -> ((i32, i32, usize), HashMap<(usize, usize), bool>) {
-    let mut obstr = HashMap::new();
+fn obstructions(text:&Vec<String>) -> ((i32, i32, usize), HashSet<(usize, usize)>) {
+    let mut obstr = HashSet::new();
     let mut start = (-1, -1, 0); // This'll get overwritten
     for (y, line) in text.iter().enumerate() {
         for (x, ch) in line.chars().enumerate() {
@@ -31,7 +30,7 @@ fn obstructions(text:&Vec<String>) -> ((i32, i32, usize), HashMap<(usize, usize)
                 start = (x as i32, y as i32, 3);
             }
             if ch == '#' {
-                obstr.insert((x, y), true);
+                obstr.insert((x, y));
             }
         }
     }
@@ -53,7 +52,7 @@ fn part1() {
     let directions = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
 
     {
-        let mut next_pos = (0, 0);
+        let mut next_pos;
     
         while {
             let (x, y, dir) = state;
@@ -70,7 +69,7 @@ fn part1() {
             // Turn if we need to
             let (x, y, dir) = state;
             let (xx, yy) = next_pos;
-            if obstr.contains_key(&(xx as usize, yy as usize)) {
+            if obstr.contains(&(xx as usize, yy as usize)) {
                 state = (x, y, (dir + 1) % 4);
                 println!("Turning at {xx} {yy} {dir}");
             } else {
@@ -87,7 +86,79 @@ fn part1() {
 
 
 fn part2() {
-    // not yet
+    // We have to add an obstruction to put the guard into a visited square.
+    // Notice -
+    // - This must be a visited square along the guard's route
+    // - Continuing the journey must reach a previously visited square (in the same direction) before it exits
+
+    let input = read_input("input.txt".to_string());
+
+    let (start, obstr) = obstructions(&input);
+    let (x0, y0, _) = start;
+
+    fn walk(start: &(i32, i32, usize), obstacles: &HashSet<(usize, usize)>, seen: &HashSet<(i32, i32, usize)>, lenx:usize, leny:usize) -> (bool, HashSet<(i32, i32, usize)>) {
+        let directions = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
+
+        let in_x_range = |x: i32| -> bool { x >= 0 && x < lenx as i32 };
+        let in_y_range = |y: i32| -> bool { y >= 0 && y < leny as i32 };
+    
+        let mut loc_seen = seen.clone();
+        let mut state = start.clone();
+        let mut next_pos;
+
+        let mut looped = false;
+    
+        while {
+        
+            if loc_seen.contains(&state) {
+                looped = true;
+            } else {
+                loc_seen.insert(state);
+            }
+
+            let (x, y, dir) = state;
+            
+            let (dx, dy) = directions[dir];
+            let xx = x + dx;
+            let yy = y + dy;
+            next_pos = (xx, yy); // Next position if we were to had in the same direction
+
+            !looped && in_x_range(xx) && in_y_range(yy) // End if looped or exited
+        } /*do*/ {
+            // Turn if we need to
+            let (x, y, dir) = state;
+            let (xx, yy) = next_pos;
+            if obstacles.contains(&(xx as usize, yy as usize)) {
+                state = (x, y, (dir + 1) % 4);
+                // println!("Turning at {xx} {yy} {dir}");
+            } else {
+                state = (xx, yy, dir);
+            }
+        }
+
+        (looped, loc_seen)
+    }
+
+    let (_, visited) = walk(&start, &obstr, &HashSet::<(i32, i32, usize)>::new(), input[0].len(), input.len());
+    let squares = visited.iter().map(|(x, y, _)| (x, y)).collect::<HashSet<_>>(); // Should be the same as part 1
+
+
+    let test_obstacle = |loc:(usize, usize)| -> bool {
+        let mut altered_obstacles = obstr.clone();
+        altered_obstacles.insert(loc);
+
+        let (result, _) = walk(&start, &altered_obstacles, &HashSet::<(i32, i32, usize)>::new(), input[0].len(), input.len());
+        result
+    };
+
+    let ans = squares.iter().filter(|&loc| {
+        let (&x, &y) = *loc;
+        (x, y) != (x0, y0) && test_obstacle((x as usize, y as usize))
+    }).count();
+
+
+
+    dbg!(ans);
 
 }
 
