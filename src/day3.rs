1edc3bb9d32a66cc11e583413d7d1cd9d5834596
William Billingsley <wbillingsley@cantab.net>
2024-12-22 13:10:38 +1100

    star 6

--- src/day3.rs
+++ src/day3.rs
@@ -17,8 +17,8 @@ fn part1() {
     let re = Regex::new(r"mul\(([0-9][0-9]?[0-9]?),([0-9][0-9]?[0-9]?)\)").unwrap();
     
     let mults = re.captures_iter(input.as_str()).map(|cap| {
-        dbg!(&cap);
-        cap[1].parse::<i64>().expect("Failed to parse {cap[1]}") * cap[2].parse::<i64>().expect("Failed to parse {cap[1]}")
+        // dbg!(&cap);
+        cap[1].parse::<i64>().expect("Failed to parse {cap[1]}") * cap[2].parse::<i64>().expect("Failed to parse {cap[2]}")
     }).collect::<Vec<_>>();
     
     dbg!(mults.into_iter().sum::<i64>());
@@ -28,7 +28,27 @@ fn part1() {
 
 
 fn part2() {
-    // Not yet
+    let input = read_input("input.txt".to_string());
+
+    // In this slightly complex regex, "do"s will be in group 1, "don't"s in group 2, "mul(x,y)"s in group3, and the extracted x,y in groups 4 and 5
+    let re = Regex::new(r"(do\(\))|(don\'t\(\))|(mul\(([0-9][0-9]?[0-9]?),([0-9][0-9]?[0-9]?)\))").unwrap();
+
+    let mut on = true;
+    
+    let mults = re.captures_iter(input.as_str()).map(|cap| {
+        dbg!(&cap);
+        if cap.get(1).is_some() {
+            on = true
+        }
+        if cap.get(2).is_some() {
+            on = false
+        }
+        if on && cap.get(3).is_some() {
+            cap[4].parse::<i64>().expect("Failed to parse {cap[1]}") * cap[5].parse::<i64>().expect("Failed to parse {cap[2]}")
+        } else { 0 }
+    }).collect::<Vec<_>>();
+    
+    dbg!(mults.into_iter().sum::<i64>());
 
 }
 
