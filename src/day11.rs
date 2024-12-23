438339f05a111100aee1d2dab341849177a9d570
William Billingsley <wbillingsley@cantab.net>
2024-12-23 21:40:39 +1100

    star 22

--- src/day11.rs
+++ src/day11.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 use num_bigint::BigUint;
 
 fn read_input(file_path: String) -> String {
@@ -60,7 +60,96 @@ fn part1() {
 
 
 fn part2() {
-    // not yet
+    let input = read_input("input.txt".to_string());
+
+    // This is a dirty cheat because the order of the numbers doesn't matter - we're just taking the count of stones
+    // and each rule only requires this number, not any of its neighbours
+
+    let numbers = input.split_whitespace().map(|s| s.to_string()).collect::<Vec<_>>();
+
+    fn parse_bi(s:&str) -> BigUint {
+        BigUint::parse_bytes(s.as_bytes(), 10).expect("Not an integer")
+    }
+
+    let mut lookup_table = HashMap::<String, Vec<String>>::new();
+
+    let mut expand_num = |s:&String| -> Vec<String> {
+
+        match lookup_table.get_mut(s) {
+            Some(v) => { v.clone() }
+            _ => {
+                let bi = parse_bi(&s);
+                if bi == BigUint::from(0 as u64) {
+                    let a = vec!["1".to_string()];
+                    lookup_table.insert(s.clone(), a.clone());
+                    a
+                } else if s.len() % 2 == 0 {
+                    let l = s.len();
+                    let (a, b) = s.split_at(l/2);
+                    let aa = parse_bi(a).to_string();
+                    let bb = parse_bi(b).to_string();
+                    let ans = vec![aa, bb];
+                    lookup_table.insert(s.clone(), ans.clone());
+                    ans
+                } else {                
+                    let mult = bi * 2024 as u16;
+                    let str = mult.to_string();
+                    let ans = vec![str];
+                    lookup_table.insert(s.clone(), ans.clone());
+                    ans
+                }
+
+            }
+
+        }        
+    };
+
+    fn to_counts(vec:&Vec<String>) -> HashMap<String, u64> {
+        let mut res = HashMap::new();
+        for s in vec.into_iter() {
+            match res.get(s) {
+                Some(n) => { res.insert(s.clone(), n + 1 as u64); }
+                None => { res.insert(s.clone(), 1 as u64); }
+            }
+        }
+        res
+    }
+
+    let counts = to_counts(&numbers);
+
+    let mut blink = |counts:HashMap<String, u64>| -> HashMap<String, u64> {
+        let mut res = HashMap::new();
+
+        for (k, v) in counts.into_iter() {
+            let blunk = expand_num(&k);
+            for s in blunk.into_iter() {
+                match res.get(&s) {
+                    Some(n) => { res.insert(s, n + v); }
+                    None => { res.insert(s, v); }
+                }
+            }
+        }
+        res
+    };
+
+    let mut blink_n = |count:u64, counts:HashMap<String, u64>| -> HashMap<String, u64> {
+        let mut res = counts;
+        for i in 0 .. count {
+            dbg!(i);
+            res = blink(res);
+        }
+        res
+    };
+
+    dbg!(&numbers);
+
+    let blunk = blink_n(75, counts);
+
+    let ans = blunk.values().into_iter().sum::<u64>();
+
+
+
+    dbg!(ans);
    
 
 }
