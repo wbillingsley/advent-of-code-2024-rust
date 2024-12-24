a5cd1814d9efd212907cc99e6d2ecdc7c7c679d9
William Billingsley <wbillingsley@cantab.net>
2024-12-24 17:51:12 +1100

    star 24

--- src/day12.rs
+++ src/day12.rs
@@ -85,7 +85,7 @@ fn regionalise(grid_size:&(usize, usize), from:&HashSet<(usize, usize)>) -> Vec<
 
 }
 
-fn perimeter(grid_size:&(usize, usize), from:&HashSet<(usize, usize)>) -> usize {
+fn perimeter_len(grid_size:&(usize, usize), from:&HashSet<(usize, usize)>) -> usize {
     let mut ans = 0;
 
     let directions = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
@@ -116,6 +116,58 @@ fn perimeter(grid_size:&(usize, usize), from:&HashSet<(usize, usize)>) -> usize 
     ans
 }
 
+// The squares of the inner perimeter, including the direction number that reached them
+fn perimeter_sqs(grid_size:&(usize, usize), from:&HashSet<(usize, usize)>) -> Vec<((i64, i64), usize)> {
+    let mut ans = Vec::new();
+
+    let directions = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
+    let perim = |fr: &(usize, usize), by: &(i64, i64)| -> Option<(i64, i64)> {
+        let (x_size, y_size) = grid_size;
+        let (dx, dy) = by;
+        let (x, y) = fr;
+
+        let xx = *x as i64 + dx;
+        let yy = *y as i64 + dy;
+        if xx >= 0 && (xx as usize) < *x_size && yy >= 0 && (yy as usize) < *y_size {
+            if from.contains(&(xx as usize, yy as usize)) { 
+                None
+            } else { Some((xx, yy)) }
+        } else { Some((xx, yy)) }
+    };
+
+    for p in from {
+        
+        directions.iter().enumerate().for_each(|(i, d)| {
+            if let Some(pp) = perim(p, &d) {
+                ans.push((pp, i));
+            }
+        });
+
+    }
+
+    ans
+}
+
+// Number of sides in a perimeter
+fn sides(perimeter:&Vec<((i64, i64), usize)>) -> usize {
+
+    // To make looking up neighbours faster
+    let as_set: HashSet<((i64, i64), usize)> = HashSet::from_iter(perimeter.into_iter().cloned());
+    let directions = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
+
+    perimeter.into_iter().filter(|(p, i)| {
+        let j = (i + 1) % 4;
+        let (dx, dy) = &directions[j];
+        let (x, y) = p;
+        let neighbour = ((x + dx, y + dy), *i);
+
+        !as_set.contains(&neighbour)
+    }).count()
+
+
+
+}
+
 
 fn part1() {
     let input = read_input("input.txt".to_string());
@@ -129,7 +181,7 @@ fn part1() {
         let rs = regionalise(&grid_size, &b);
         rs.into_iter().map(|r| {
             let area = r.len();
-            let perim = perimeter(&grid_size, &r);
+            let perim = perimeter_len(&grid_size, &r);
             println!(" {area} * {perim} ");
             area * perim
         }).sum::<usize>()
@@ -143,12 +195,34 @@ fn part1() {
 
 fn part2() {
 
-    // not yet
+    let input = read_input("input.txt".to_string());
+    let grid = &input.into_iter().map(|line| line.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+
+    let grid_size = (grid[0].len(), grid.len());
+    let buckets = buckets(&grid);
+
+    let regions = buckets.into_iter().map(|(ch, b)| {
+        println!("Processing {ch}");
+        let rs = regionalise(&grid_size, &b);
+        rs.into_iter().map(|r| {
+            let area = r.len();
+            let perim_sqs = perimeter_sqs(&grid_size, &r);
+            let sides = sides(&perim_sqs);
+
+
+
+            println!(" {area} * {sides} ");
+            area * sides
+        }).sum::<usize>()
+
+    }).sum::<usize>();
+
+    dbg!(&regions);
 
 }
 
 pub fn day12() {
-    part1();
+    // part1();
     part2();
 }
 
