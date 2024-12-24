a4d73964c8a69bf0fc7cbd9629e2e4afb2e0ad77
William Billingsley <wbillingsley@cantab.net>
2024-12-25 00:32:24 +1100

    star 28

--- src/day14.rs
+++ src/day14.rs
@@ -93,7 +93,65 @@ fn part1() {
 
 fn part2() {
     
-    // not yet
+    let input = read_input("input.txt".to_string());
+    let re = Regex::new(r"(-?[0-9]+)").unwrap();
+
+    let w = 101 as i64;
+    let h = 103 as i64;
+
+    let mut robots = input.into_iter().map(|line| {
+        let nums = re.captures_iter(&line).map(|cap| cap[0].parse::<i64>().expect("Failed to parse {num}")).collect::<Vec<_>>(); 
+        Robot {
+            p: Vec2d { x: nums[0], y: nums[1] },
+            v: Vec2d { x: nums[2], y: nums[3] }
+        }
+    }).collect::<Vec<Robot>>();
+
+
+    let picture = |robots:&Vec<Robot>| -> String {
+        let mut s = String::new();
+
+        for y in 0..h {
+            let mut line = String::new();
+
+            for x in 0..w {
+                let count = robots.into_iter().filter(|r| r.p.x == x && r.p.y == y).count();
+                let c = if count == 0 { "." } else { &count.to_string() };
+                line.push_str(&c);
+            }
+
+            line.push('\n');
+            s.push_str(&line);
+        }
+        s.push('\n');
+        s
+    };
+
+
+    // let middle = Quad {
+    //     tl: Vec2d { x: w/2 - 2, y: h/2 - 2 },
+    //     br: Vec2d { x: w/2 + 2, y: h/2 + 2 }
+    // };
+
+    for i in 1..100000 {
+        for r in robots.iter_mut() {
+            r.step(&Vec2d{ x: w, y: h });
+        }
+
+        // If there's a lot of robots in the middle of the picture, print it
+        // let in_middle = robots.iter().filter(|r| r.is_in(&middle)).count();
+        // in_middle failed because the tree's not in the middle of the picture. Oh well.
+
+        if i % 10000 == 0 {
+            dbg!(i);
+        }
+
+        if i % 101 == 85 { // From eyeballing the first 100 patterns and seeing where they group
+            let p = picture(&robots); 
+            println!("{i}");
+            println!("{p}");    
+        }
+    }
 
 }
 
