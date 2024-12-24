927bb264852f1b712996a936f0124712f17177fc
William Billingsley <wbillingsley@cantab.net>
2024-12-24 22:38:48 +1100

    star 26

--- src/day13.rs
+++ src/day13.rs
@@ -61,6 +61,61 @@ fn part1() {
 
 
 fn part2() {
+    let input = read_input("input.txt".to_string());
+    let re = Regex::new(r"([0-9]+)").unwrap();
+
+    // The pattern is three lines of info, 1 lone of blank
+    let mins = input.chunks(4).map(|block| {
+        let aa = re.captures_iter(&block[0]).map(|cap| cap[0].parse::<i64>().expect("Failed to parse {num}")).collect::<Vec<_>>(); // button a
+        let bb = re.captures_iter(&block[1]).map(|cap| cap[0].parse::<i64>().expect("Failed to parse {num}")).collect::<Vec<_>>(); // button b
+        let cc = re.captures_iter(&block[2]).map(|cap| cap[0].parse::<i64>().expect("Failed to parse {num}")).collect::<Vec<_>>(); // prize
+
+        let ax = aa[0];
+        let ay = aa[1];
+        let bx = bb[0];
+        let by = bb[1];
+        let tx =  10000000000000 + cc[0];
+        let ty =  10000000000000 + cc[1];
+
+        // a * ax + b * bx = tx
+        // a * ay + b * by = ty 
+
+        // a = (tx - b * bx) / ax
+        // a = (ty - b * by) / ay
+
+        // (tx - b * bx) / ax = (ty - b * by) / ay
+
+        // (tx - b * bx) * ay = (ty - b * by) * ax
+
+        // ay * tx - b * bx * ay = ax * ty - b * by * ax
+
+        // b * by * ax - b * bx * ay = ax * ty - ay * tx
+
+        // b (by * ax - bx * ay) = ax * ty - ay * tx
+
+        // b = (ax * ty - ay * tx) / (by * ax - bx * ay)
+
+        let b = (ax * ty - ay * tx) / (by * ax - bx * ay);
+        
+        let a = (ty - b * by) / ay; 
+
+        if a * ax + b * bx == tx {
+            println!("{a} {b}");
+            Some(3 * a + b)
+        } else { None }
+        
+    });
+
+    let mut tot = 0 as i64;
+    for opt in mins {
+        dbg!(&opt);
+        if let Some(v) = opt {
+            tot += v;
+        }
+    }
+
+
+    dbg!(tot);
     
     // not yet
 
