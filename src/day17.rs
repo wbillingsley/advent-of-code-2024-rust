e1ba567e560998f4d7e583644093513f3a8853cb
William Billingsley <wbillingsley@cantab.net>
2024-12-27 16:30:16 +1100

    star 34

--- src/day17.rs
+++ src/day17.rs
@@ -1,5 +1,7 @@
 use std::fs;
 use regex::Regex;
+use std::collections::HashSet;
+
 
 fn read_input(file_path: String) -> Vec<String> {
     println!("Reading input");
@@ -35,7 +37,7 @@ impl MachineState {
         }
     }
 
-    fn inst(&self, prog:&Vec<usize> ) -> MachineState {
+    fn inst(&self, prog:&Vec<usize> ) -> (Option<i64>, MachineState) {
         let op = prog[self.instruction];
         match op {
             0 => {
@@ -44,11 +46,11 @@ impl MachineState {
                 let denom = (2 as i64).pow(self.combo(prog[self.instruction + 1]) as u32);
                 let res = num / denom;
                 
-                MachineState { 
+                (None, MachineState { 
                     instruction: self.instruction + 2,
                     a: res,
                     ..*self
-                }
+                })
             },
 
             1 => {
@@ -56,11 +58,11 @@ impl MachineState {
                 let arg = prog[self.instruction + 1];
                 let res = self.b as usize ^ arg;
 
-                MachineState { 
+                (None, MachineState { 
                     instruction: self.instruction + 2,
                     b: res as i64,
                     ..*self
-                }
+                })
             },
 
             2 => {
@@ -68,26 +70,26 @@ impl MachineState {
                 let comb = self.combo(prog[self.instruction + 1]);
                 let res = comb % 8;
 
-                MachineState {
+                (None, MachineState {
                     instruction: self.instruction + 2,
                     b: res as i64,
                     ..*self
-                }
+                })
 
             },
 
             3 => {
                 // jnz
                 if self.a == 0 {
-                    MachineState {
+                    (None, MachineState {
                         instruction: self.instruction + 2,                    
                         ..*self
-                    }
+                    })
                 } else {
-                    MachineState {
+                    (None, MachineState {
                         instruction: prog[self.instruction + 1],                    
                         ..*self
-                    }
+                    })
                 }
             },
 
@@ -95,22 +97,22 @@ impl MachineState {
                 // bxc
                 let res = self.b ^ self.c;
 
-                MachineState {
+                (None, MachineState {
                     instruction: self.instruction + 2,
                     b: res as i64,
                     ..*self
-                }
+                })
             },
 
             5 => {
                 // out
                 let comb = self.combo(prog[self.instruction + 1]) % 8;
-                print!("{comb},");
+                // print!("{comb},");
 
-                MachineState {
+                (Some(comb), MachineState {
                     instruction: self.instruction + 2,
                     ..*self
-                }
+                })
             },
 
             6 => {
@@ -119,11 +121,11 @@ impl MachineState {
                 let denom = (2 as i64).pow(self.combo(prog[self.instruction + 1]) as u32);
                 let res = num / denom;
                 
-                MachineState { 
+                (None, MachineState { 
                     instruction: self.instruction + 2,
                     b: res,
                     ..*self
-                }
+                })
             },
 
             7 => {
@@ -132,11 +134,11 @@ impl MachineState {
                 let denom = (2 as i64).pow(self.combo(prog[self.instruction + 1]) as u32);
                 let res = num / denom;
                 
-                MachineState { 
+                (None, MachineState { 
                     instruction: self.instruction + 2,
                     c: res,
                     ..*self
-                }
+                })
             }
 
 
@@ -165,8 +167,13 @@ fn part1() {
 
     let mut cursor = start;
     while cursor.instruction < prog.len() {
-        dbg!(cursor.instruction);
-        cursor = cursor.inst(&prog);
+        let opt;
+        // dbg!(cursor.instruction);
+        (opt, cursor) = cursor.inst(&prog);
+        
+        if let Some(n) = opt {
+            print!("{n},");
+        }
     }
 
 
@@ -176,7 +183,103 @@ fn part1() {
 }
 
 fn part2() {
-    // not yet    
+
+    // To work this one out, you have to reverse engineer YOUR program. (An arbitrary program might not terminate, might move to an odd instruction index, etc)
+    // 
+    // 2,4, 1,7, 7,5, 1,7, 4,6, 0,3, 5,5, 3,0 (spaces inserted for ease of reading)
+
+    // 3,0 at end loops to the start.
+    // there is one 5 (out) per loop.
+
+    // a is shifted right 3 bits per loop. So, a has at least 48 bits (probably - unless b unwinding against zeros proks for the last few numbers)
+
+    // Figure out the first loop 
+    // 2,4  b = lowest 3 bits of A
+    // 1,7 b = ~b
+    // 7,5 c = a/b
+    // 1,7 b = ~b
+    // 4,6 b = b xor c
+    // 0,3 a = a >> 3
+    // 5,5 out(b)
+    // 3,0 loop
+
+    // So if our first number is 2, we should be able to figure out the lowest bits of A.
+
+    // take lowest 3 bits
+    // invert them
+    // shift the num right that many bits
+    // xor
+
+    // i.e. "the next 3 bits up the chain, shifted right by these 3 bits inverse, differ by 2, 4, 1, etc."
+
+    
+    // fn test(a:usize, target:usize) -> bool {
+    //     let b = a % 8;
+    //     let c = a >> ((!b) % 8);
+    //     let xored = (b ^ c) % 8;
+    //     xored == target
+    // }
+
+    type Candidate = (usize, usize);
+
+    // We can only join parts of candidates if they agree on any overlap in their masks
+    fn join(a:&Candidate, b:&Candidate) -> Option<Candidate>  {
+        let (aa, amask) = a;
+        let (bb, bmask) = b;
+
+        let cc = aa | bb;
+        let cmask = amask | bmask;
+
+        if (cc & amask == *aa) && (cc & bmask == *bb) {
+            Some((cc, cmask))
+        } else { None }
+    }
+
+    // Let' try generating possibilites and hope the constraints narrow it down
+
+    fn generate_candidates_at(incoming:&Candidate, lshift:usize, xor:usize) -> HashSet<Candidate> {
+
+        let parts = (0..8).filter_map(|oct| {
+            let shift = !oct & 7;
+            let low_bits = oct << lshift;
+            let low_mask = 7 << lshift;
+            let high_bits = (oct ^ xor) << (lshift + shift);
+            let high_mask = 7 << (lshift + shift); // Sets the lowest 3 bits plus the comparison to whichever higher bits
+            
+            join(&(low_bits, low_mask), &(high_bits, high_mask)) 
+        });
+
+        parts.filter_map(|c| join(&incoming, &c)).collect::<HashSet<Candidate>>()
+    }
+
+    let prog: Vec<usize> = vec![2,4, 1,7, 7,5, 1,7, 4,6, 0,3, 5,5, 3,0];
+
+    let mut cands = HashSet::<Candidate>::from([(0,0)]);
+
+    for (i, x) in prog.iter().enumerate() {
+        dbg!(i, cands.len());
+        let mut next = HashSet::<Candidate>::new();
+
+        for c in cands.iter() {
+            let ccs = generate_candidates_at(c, 3 * i, *x);
+            for cc in ccs {
+                next.insert(cc);
+            }
+        }
+
+        cands = next;
+    }
+
+
+    let lowest = cands.iter().map(|(c, _)| c).min();
+
+
+    dbg!(lowest);
+
+
+    // test_num(117440, &base, &prog);
+
+
 
 }
 
